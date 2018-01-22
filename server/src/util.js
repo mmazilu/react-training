1c83f229093ddeca0a472468d24e4d1bec9e1307
Valentina <miron.vale@gmail.com>
2018-01-22 12:11:31 +0200

    server side

---
+++ server/src/util.js
@@ -0,0 +1,17 @@
+// server/src/util.js
+export function chain (middlewares) {
+    return (req, res) => {
+        [...middlewares] // need to copy array because #reverse mutates it
+            .reverse()
+            .reduce(
+                (next, middleware) => {
+                    return err => {
+                        // if next was called with an err, skip all middlewares and call the final one, whose next is null
+                        if (err && next) { return next(err) }
+                        middleware(req, res, next, err)
+                    }
+                },
+                null
+            )()
+    }
+}
---
+++ server/webpack.config.js
@@ -0,0 +1,29 @@
+// server/webpack.config.js
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const { DefinePlugin } = require('webpack')
+
+const config = {
+    target: 'node',
+    entry: './index',
+    context: path.resolve(__dirname, 'src'),
+    output: {
+        filename: 'index.js',
+        path: path.join(__dirname, 'build')
+    },
+    module: {
+        loaders: [
+            { test: /\.json$/, loader: 'json-loader' }
+        ]
+    },
+    plugins: [
+        new DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ],
+    resolve: { extensions: ['.js', '.json'] }
+}
+
+config.externals = [nodeExternals()]
+
+module.exports = config
